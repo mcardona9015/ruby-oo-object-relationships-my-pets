998c93af6faf7b01411666c5773155702da20bc6
mcardona9015 <mcardona9015@gmail.com>
2020-12-10 18:17:28 -0500

    Finishedgit add .

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,21 +29,67 @@ class Owner
    def cats
     cats = []
     Cat.all.each do |cat| 
-      if cat.owner == self.name
+      if cat.owner == self
          cats << cat
-#         binding.pry
+        #binding.pry
       end
     end
     cats
-#    binding.pry
-    # if Cat.all.select{|cat| cat.owner == self.name}
-    # cats << Cat.all.select{|cat| cat.owner == self.name}
    end
+
+   def dogs
+    dogs = []
+    Dog.all.each do |dog| 
+      if dog.owner == self
+         dogs << dog
+        #binding.pry
+      end
+    end
+    dogs
+   end
+
+   def buy_dog(name)
+    Dog.new(name, self)
+   end
+
+   def buy_cat(name)
+    Cat.new(name, self)
+   end
+
+   def walk_dogs
+    Dog.all.each do |dog|
+      dog.mood = "happy"
+    end
+   end
+
+   def feed_cats
+    Cat.all.each do |cat|
+      cat.mood = "happy"
+    end
+   end
+
+   def sell_pets
+    Dog.all.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+    Cat.all.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+  end
+
+    def list_pets
+      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    end
+
+
 
 
 end
 
 # tom = Owner.new("Tom")
-# garfield = Cat.new("Garfield", "Tom")
-# #binding.pry
-#0
+# garfield = Cat.new("Garfield", tom)
+# maru = Cat.new("Maru", tom)
+# binding.pry
+# 0
